7f6653704c4575ef951756b50f758b1ba1ef6d97
harshpatidar245 <harshpatidar2400@gmail.com>
2024-11-16 11:49:25 +0530

    Initial commit

--- urban/src/Components/Product.jsx
+++ urban/src/Components/Product.jsx
@@ -13,84 +13,25 @@ import {
 import { FaStar } from "react-icons/fa";
 import { cartContext } from "../Context/CartContextProvider";
 
+// Product data
 const data = [
   {
     id: 1,
-    title: "First floor cleaning",
+    title: "Floor Cleaning",
     rating: "4.77 (636)",
-    price: "99",
-    src: "https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1644931257406-6b799b.jpeg ",
+    price: 150,
+    phone: "9826506683",
+    src: "https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1644931257406-6b799b.jpeg",
   },
   {
     id: 2,
-    title: "First floor cleaning",
+    title: "Bathroom Cleaning",
     rating: "4.77 (636)",
-    price: "99",
-    src: "https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1644931257406-6b799b.jpeg ",
+    price: 99,
+    phone: "9826506683",
+    src: "https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1644931257406-6b799b.jpeg",
   },
-  {
-    id: 3,
-    title: "First floor cleaning",
-    rating: "4.77 (636)",
-    price: "99",
-    src: "https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1644931257406-6b799b.jpeg ",
-  },
-  {
-    id: 4,
-    title: "First floor cleaning",
-    rating: "4.77 (636)",
-    price: "99",
-    src: "https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1644931257406-6b799b.jpeg ",
-  },
-  {
-    id: 5,
-    title: "First floor cleaning",
-    rating: "4.77 (636)",
-    price: "99",
-    src: "https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1644931257406-6b799b.jpeg ",
-  },
-  {
-    id: 6,
-    title: "First floor cleaning",
-    rating: "4.77 (636)",
-    price: "99",
-    src: "https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1644931257406-6b799b.jpeg ",
-  },
-  {
-    id: 7,
-    title: "First floor cleaning",
-    rating: "4.77 (636)",
-    price: "99",
-    src: "https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1644931257406-6b799b.jpeg ",
-  },
-  {
-    id: 8,
-    title: "First floor cleaning",
-    rating: "4.77 (636)",
-    price: "99",
-    src: "https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1644931257406-6b799b.jpeg ",
-  },
-  {
-    id: 9,
-    title: "First floor cleaning",
-    rating: "4.77 (636)",
-    price: "99",
-    src: "https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1644931257406-6b799b.jpeg ",
-  },
-  {
-    id: 10,
-    title: "First floor cleaning",
-    rating: "4.77 (636)",
-    price: "99",
-    src: "https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1644931257406-6b799b.jpeg ",
-  },
-  {
-    id: 11,
-    title: "First floor cleaning",
-    rating: "4.77 (636)",
-    price: "99",
-    src: "https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/w_64,dpr_1,fl_progressive:steep,q_auto,c_limit/images/growth/home-screen/1644931257406-6b799b.jpeg ",
-  },
+  // Add more items...
 ];
 
 const Product = () => {
@@ -99,7 +40,7 @@ const Product = () => {
   return (
     <div>
       <Flex w={"90%"} borderTop={"1px"} m={"auto"} justify="space-between">
-        {/* left side */}
+        {/* Left Side */}
         <Box px={10}>
           {data.map((item) => (
             <Box
@@ -112,31 +53,31 @@ const Product = () => {
               style={{ boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" }}
             >
               <HStack justify="space-between">
-                {/* left */}
-                <VStack>
-                  <Text>{item.title}</Text>
+                {/* Item Details */}
+                <VStack align="flex-start">
+                  <Text fontWeight="bold">{item.title}</Text>
                   <HStack>
-                    <FaStar />
-                    <Text>4.82 (139K)</Text>
+                    <FaStar color="gold" />
+                    <Text>{item.rating}</Text>
                   </HStack>
-                  <Text>₹ {item.price}</Text>
+                  <Text>₹{item.price}</Text>
                 </VStack>
 
-                {/* right */}
-
-                <Image mr={40} src={item.src} mb={3} />
+                {/* Item Image */}
+                <Image src={item.src} alt={item.title} boxSize="80px" />
               </HStack>
-              <Button onClick={() => handleTotal(item.price)}>Add</Button>
+              <Button mt={3} colorScheme="teal" onClick={() => handleTotal(item.price)}>
+                Add to Cart
+              </Button>
             </Box>
           ))}
         </Box>
 
-        {/* right side */}
-
+        {/* Right Side */}
         <Box w={"500px"}>
           <Stack align={"center"} mt={100}>
-            <Heading>Total : {total}</Heading>
-            <Button w={"60%"} mt={"50px"}>
+            <Heading>Total: ₹{total}</Heading>
+            <Button w={"60%"} mt={5} colorScheme="teal">
               Checkout
             </Button>
           </Stack>
